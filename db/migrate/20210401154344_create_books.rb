f9097c34336f61a027549262ad4902604f621ed3
ShuopengLiu <shuopengliu@gmail.com>
2021-04-01 10:44:43 -0500

    Correct book model

--- db/migrate/20210401154344_create_books.rb
+++ db/migrate/20210401154344_create_books.rb
@@ -1,7 +1,6 @@
-class CreateProducts < ActiveRecord::Migration[6.1]
+class CreateBooks < ActiveRecord::Migration[6.1]
   def change
-    create_table :products do |t|
-      t.string :book
+    create_table :books do |t|
       t.string :title
       t.string :author
       t.string :publisher
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_01_153211) do
+ActiveRecord::Schema.define(version: 2021_04_01_154344) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -36,6 +36,17 @@ ActiveRecord::Schema.define(version: 2021_04_01_153211) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_admin_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
+  end
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.string "author"
+    t.string "publisher"
+    t.text "description"
+    t.string "condition"
+    t.decimal "price"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "genres", force: :cascade do |t|
