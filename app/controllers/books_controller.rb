0be3e38a1fbbf1161c0214d461142c203ed1c24f
ShuopengLiu <shuopengliu@gmail.com>
2021-04-01 13:06:02 -0500

    Views edited

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,10 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all
+  end
+  # GET /books/:id
+  def show
+    @book = Book.find(params[:id])
   end
 
-  def show
-  end
 end
--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,7 +1,12 @@
 class GenresController < ApplicationController
+  # GET /
+  # GET /genres
   def index
+    @genres = Genre.all
   end
 
+  # GET /genres/:id
   def show
+    @genre = Genre.find(params[:id])
   end
 end
