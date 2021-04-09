a0c030e7b425225965dc488acb853fa73ce9db12
ShuopengLiu <shuopengliu@gmail.com>
2021-04-09 11:11:50 -0500

    Removed column email form users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_01_192255) do
+ActiveRecord::Schema.define(version: 2021_04_09_160725) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -119,7 +119,6 @@ ActiveRecord::Schema.define(version: 2021_04_01_192255) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.string "email"
     t.string "phone"
     t.text "address"
     t.datetime "created_at", precision: 6, null: false
