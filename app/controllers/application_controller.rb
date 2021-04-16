73c1ef47040344782c9cfa09b8565378d6d7a7f8
ShuopengLiu <shuopengliu@gmail.com>
2021-04-16 01:50:09 -0500

    session cart quantity bug fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,20 +1,22 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!, :initialize_session
+  before_action :authenticate_user!
+  before_action :initialize_session
   helper_method :cart, :quantity
 
   private
 
   def initialize_session
-    session[:shopping_cart] ||= []
+    # Create a shopping cart in form of hash
+    session[:shopping_cart] ||= {}
   end
 
   def cart
     # .find will take in a single value OR an array of values
     # with an array, it will bring back a collection of objects based on those PK id's
-    Book.find(session[:shopping_cart])
+    Book.find(session[:shopping_cart].keys)
   end
 
   def quantity
-    session[:id]
+    session[:shopping_cart][:id]
   end
 end
--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,25 +1,25 @@
 class CartController < ApplicationController
   def create
     # Add param[:id] to the cart
-    # logger.debug("Adding #{params[:id]} to the cart!")
-    id = params[:id].to_i
-    if session[:shopping_cart].include?(id)
-      session[:id] += 1
+
+    id = params[:id]
+    logger.debug("LSP: #{session[:shopping_cart].keys.inspect}")
+    if session[:shopping_cart].keys.include?(id)
+      session[:shopping_cart][id] += 1
     else
-      session[:shopping_cart] << id
-      session[:id] = 1
+      session[:shopping_cart][id] = 1
     end
 
-    book = Book.find(id)
+    book = Book.find(id.to_i)
     flash[:notice] = "+ #{book.title} added to cart."
     redirect_to root_path
   end
 
   def destroy
     # remove param[:id] from cart
-    id = params[:id].to_i
+    id = params[:id]
     session[:shopping_cart].delete(id)
-    book = Book.find(id)
+    book = Book.find(id.to_i)
     flash[:notice] = "- #{book.title} removed from cart."
     redirect_to root_path
   end
