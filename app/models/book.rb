5ae5b01cdc21572621e4d74c5ea02566e68e0e8f
ShuopengLiu <shuopengliu@gmail.com>
2021-04-22 01:46:50 -0500

    Generated order controller

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,7 +2,7 @@ class Book < ApplicationRecord
   paginates_per 9
   belongs_to :genre
 
-  has_many :ordered_books, dependent: :destory
+  has_many :ordered_books, dependent: :destroy
   has_many :orders, through: :ordered_books
 
   validates :title, :price, :author, :publisher, presence: true
